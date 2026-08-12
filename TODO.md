423591a46bd69db20a8f2239b5e42546b61d8870
Mauricio Florez <florexmauricio15@gmail.com>
2026-08-11 19:36:04 -0500

    Imporataciones para configuracion de corns para conectar backend con frontend

--- TODO.md
+++ TODO.md
@@ -71,4 +71,4 @@
                                                            │
                                                            ▼
                                                     [ SQLite DB ]
-                                                 (persistencia real)
+                                                    (persistencia real)
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,9 @@ from contextlib import asynccontextmanager
 from app.database import init_db
 from fastapi import FastAPI
 from app.routers import character, quests
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +15,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan, title="Life-OS API")
 app.include_router(character.router)
 app.include_router(quests.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
