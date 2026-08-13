a726c3c240c3642a2b8b6fa9f7333ba64a59a1f4
Keiner Buitron <buitronkeiner75@gmail.com>
2026-08-12 21:24:07 -0500

    Se modifica la direccionp ip para ejecutar en un dispositivo externo

--- android/app/src/main/java/com/tu_nombre/lifeos/network/RetrofitClient.java
+++ android/app/src/main/java/com/tu_nombre/lifeos/network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     // 10.0.2.2 es el alias para localhost de tu PC desde el emulador de Android Studio.
-    private static final String BASE_URL = "http://10.0.2.2:8000/";
+    private static final String BASE_URL = "http://192.168.56.1/";
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
